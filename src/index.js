70027d748d52d6a0f8bb898b07498ea357387621
enjoytime1101-spec <enjoytime1101@gmail.com>
2026-09-09 12:54:47 +0800

    chore: add src/index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { auditPath, auditSource, formatAudit } from "./audit.js";
+export { runChecks } from "./check.js";
+export { iconNames, icons, renderIcon } from "./icons.js";
+export { applyMigration, planMigration } from "./migrate.js";
+export { normalizeLabel, policy, suggestButton } from "./policy.js";
